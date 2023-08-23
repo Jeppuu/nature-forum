4b772637eeb9d88f569ac732e4890a6864c5860e
jeppuu <jennamariakoskelo@gmail.com>
2023-08-23 08:13:31 +0300

    API fix

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -23,7 +23,7 @@ export default function Register() {
       return
     }
     try {
-      const res = await fetch('http://localhost:3000/api/register', {
+      const res = await fetch('/api/register', {
         headers: {
           'Content-Type': 'application/json'
         },
